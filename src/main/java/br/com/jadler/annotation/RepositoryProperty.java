a083cb7821592454f9bf39c31e2a196141b53de0
Jaguaraquem Adler Reinaldo <jaguar.adler@gmail.com>
2018-10-18 01:25:35 -0300

    Created annotation class for spring repositories;
    This is a fork of an old project. This annotation will be used to
    annotate Entity classes in the star-wars project that should have a
    Repository class associated.

---
+++ src/main/java/br/com/jadler/annotation/RepositoryProperty.java
@@ -0,0 +1,18 @@
+package br.com.jadler.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @since 1.0
+ * @version 1.0
+ * @author <a href="mailto:jaguar.adler@gmail.com">Jaguaraquem A. Reinaldo</a>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface RepositoryProperty {
+
+}
