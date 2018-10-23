957aa26685088bf77c2f29fe5bf8b60251a48850
Jaguaraquem Adler Reinaldo <jaguar.adler@gmail.com>
2018-10-23 00:08:36 -0300

    Added ControllerProperty and MappedProperty;
    Added ControllerPropert and MappedProperty annottions and controler.ftl
    freemarker template.

---
+++ src/main/java/br/com/jadler/annotation/MappedProperty.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.jadler.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @since 1.0
+ * @version 1.0
+ * @author <a href="mailto:jaguar.adler@gmail.com">Jaguaraquem A. Reinaldo</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface MappedProperty {
+}
